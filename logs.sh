d3816ca52ccb01afe339cdc48bb082ac3bd78fc0
Joe Sussman <jsussman@neverware.com>
2018-01-28 07:21:39 -0500

    logs.sh: download, decompress and unarchive a cloudready log package

---
+++ logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+: '
+ Download, decompress and unarchive a CloudReady log package.
+
+Also changes removes both the intermediate tarball and the
+compressed tarball.
+'
+
+set -e
+
+
+logs=$1
+
+if [[ -z $logs ]]; then
+    script=$(basename "$0")
+    printf "%s: missing log package name\n" "$script"
+    exit 1
+fi
+
+get_logs () {
+    eval "aws s3 cp s3://cloudready-logs/$logs ."
+    lz4 -df "$logs"
+    
+    tarball=${logs%.*}
+    tar xf "$tarball"
+
+    rm -f "$logs" "$tarball"
+    
+    logsdir=${tarball%.*}
+    change_dir "$logsdir"
+}
+
+get_logs;
